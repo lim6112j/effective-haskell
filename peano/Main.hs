9ff2b83456b1722c34dad4d5de6220f5eab9c502
ben <Lim6112j@gmail.com>
2025-01-13 11:53:50 +0900

    inductively defined list

--- peano/Main.hs
+++ peano/Main.hs
@@ -44,6 +44,43 @@ fromList :: List a -> [a]
 fromList Empty = []
 fromList (Cons x xs) = x : fromList xs
 
+fromList' :: List a -> [a]
+fromList' = listFoldr (:) []
+listFoldr :: (a -> b -> b) -> b -> List a -> b
+listFoldr _ b Empty = b
+listFoldr f b (Cons x xs) =
+  f x $ listFoldr f b xs
+
+fromListl :: List a -> [a]
+fromListl = listFoldl appendElement []
+appendElement :: [a] -> a -> [a]
+appendElement b a = b ++ [a]
+listFoldl :: (b -> a -> b) -> b -> List a -> b
+listFoldl _ b Empty = b
+listFoldl f b (Cons x xs) =
+  let b' = b `f` x
+  in listFoldl f b' xs
+
+listHead :: List a -> Maybe a
+listHead l =
+  case l of
+    (Cons x _) -> Just x
+    _ -> Nothing
+listTail :: List a -> List a
+listTail l =
+  case l of
+    Empty -> Empty
+    Cons _ xs -> xs
+listReverse :: List a -> List a
+listReverse l =
+  case fromList l of
+    [] -> Empty
+    (x:xs) -> toList $ fromList (listReverse $ toList xs) ++ [x]
+
+listMap :: (a -> b) -> List a -> List b
+listMap _ Empty = Empty
+listMap f (Cons x xs) =
+  Cons (f x) (listMap f xs)
 aList :: List Int
 aList = toList [1, 2, 3, 4, 5]
 
@@ -57,3 +94,6 @@ main = do
   print $ subPeano cPeano bPeano
   print $ addPeano aPeano bPeano
   print $ fromList aList
+  print $ fromListl aList
+  print $ listReverse aList
+  print $ listMap (* 2) aList
