ccf8a0f73bff5f20dfc555ca5a61f289d60d86b2
byeongcheollim <lim6112j@gmail.com>
2025-01-22 17:53:15 +0900

    handle error

--- pager/Hcat.hs
+++ pager/Hcat.hs
@@ -2,6 +2,7 @@
 module Hcat(runHCat) where
 import qualified System.Environment as Env
 import Prelude hiding (FilePath)
+import qualified Control.Exception as Exception
 getArgs :: IO [String]
 getArgs = Env.getArgs
 type FilePath = String
@@ -12,8 +13,13 @@ handleArgs = getArgs >>= \case
     _ -> return $ Left "Usage: hcat <file>"
 
 runHCat :: IO ()
-runHCat = handleArgs >>= \case
+runHCat = Exception.catch
+  ( handleArgs >>= \case
   Left err ->
     putStrLn $ "Error Processing: " <> err
   Right file ->
     readFile file >>= putStrLn
+  ) handleErr
+  where
+    handleErr :: Exception.IOException -> IO ()
+    handleErr e = putStrLn $ "Error: " <> show e
