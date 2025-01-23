934b93418de0f02bcf6ebb09bf85da056fab35fe
byeongcheollim <lim6112j@gmail.com>
2025-01-24 01:02:15 +0900

    pager

--- pager/Hcat.hs
+++ pager/Hcat.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE LambdaCase #-}
-module Hcat(runHCat, runHCat', runHCat'') where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Hcat(runHCat, runHCat', runHCat'', groupsOf, wordWrap) where
 import qualified System.Environment as Env
 import Prelude hiding (FilePath)
 import qualified Control.Exception as Exception
 import qualified System.IO.Error as IOError
 import qualified Data.Text.IO as TextIO
+import qualified Data.Text as Text
 getArgs :: IO [String]
 getArgs = Env.getArgs
 type FilePath = String
@@ -65,3 +68,14 @@ runHCat''' =
     handleIOError io = Exception.catch io handleErr
     handleErr :: IOError -> IO ()
     handleErr e = putStrLn $ "Error: " <> show e
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf _ [] = []
+groupsOf n xs =
+  let (first, rest) = splitAt n xs
+  in first : groupsOf n rest
+wordWrap :: Int -> Text.Text -> [Text.Text]
+wordWrap n  txt
+  | Text.length txt <= n = [txt]
+  | otherwise =
+        let (line, rest) = Text.splitAt n txt
+        in line : wordWrap n rest
