8fbc10c27ed1fafd136c154206e1c52dd9ca3a4e
byeongcheollim <lim6112j@gmail.com>
2025-01-28 18:07:48 +0900

    ...

--- chapter9/Main.hs
+++ chapter9/Main.hs
@@ -1,4 +1,5 @@
 module Main(main) where
+import Text.Read (readMaybe)
 newtype Function a b = Function { runFunction :: a -> b }
 instance Functor (Function a) where
   fmap f (Function g) = Function $ f . g
@@ -25,5 +26,20 @@ instance Monad List where
   return a = List a Empty
   Empty >>= _ = Empty
   List a rest >>= f = f a `mappend` (rest >>= f)
+
+-- return half of a value if it's even, otherwise Nothing
+half :: Int -> Maybe Int
+half n
+  | even n = Just (n `div` 2)
+  | otherwise = Nothing
+-- takes a boundary. Returns Just the value if it's within the range.
+-- and Nothing otherwise
+bound :: (Int, Int) -> Int -> Maybe Int
+bound (min, max) n =
+  if (n >= min) && (n <= max)
+  then Just n
+  else Nothing
+-- Every function returns a Just value
+  
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
